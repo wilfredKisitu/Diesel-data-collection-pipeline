700040e73af148dcd7db6e675ea6976bbc116387
wilfredKisitu <wktdcourses@gmail.com>
2026-03-20 10:30:09 +0300

    added support for device type

---
+++ devices/devices.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+class Device(Enum):
+    SERIAL = 1
+    MODBUS = 2
+    CANBUS = 3
+    FUTURE_PROTOCOLS = 4
+    UNKNOWN = 0
+
+    @classmethod
+    def _missing_(cls, value):
+        return cls.UNKNOWN
+
+    @property
+    def get_device_name(self):
+        mapping = {
+            Device.SERIAL: 'Serial',
+            Device.MODBUS: 'Modbus',
+            Device.CANBUS: 'Can bus',
+            Device.FUTURE_PROTOCOLS: 'Futue protocols',
+            Device.UNKNOWN: 'Unsupported protocol'
+        }
+        return mapping.get(self)
+
+        
+
+
+if __name__ == '__main__':
+    print('Testing Device types')
+
+    protocol = Device(5)
+    print(protocol.get_device_name)
+
+    print('Test passed')
+
+
+
